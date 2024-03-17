5093d64950a29fed58c66a63363918035092a8d7
Elton Irokromo <144001751+RedDragg@users.noreply.github.com>
2024-03-17 20:59:39 +0100

    Add files via upload

---
+++ copytext.js
@@ -0,0 +1,18 @@
+const mailParagraph = document.querySelector(".mail"); // Selecteer het element met class "mail"
+const copyMessage = document.getElementById("copyMessage"); // Selecteer het element voor het kopieerbericht
+
+mailParagraph.onclick = function() {
+    document.execCommand("copy");
+}
+
+mailParagraph.addEventListener("copy", function(event) {
+    event.preventDefault();
+    if (event.clipboardData) {
+        event.clipboardData.setData("text/plain", mailParagraph.textContent);
+        console.log(event.clipboardData.getData("text"))
+        copyMessage.style.display = "block"; // Toon het kopieerbericht
+        setTimeout(function() {
+            copyMessage.style.display = "none"; // Verberg het kopieerbericht na enkele seconden
+        }, 2000); // Hier kun je de weergavetijd van het bericht aanpassen (in milliseconden)
+    }
+});
